fc2f38d57db8d098729d0a1a083b1196520d5030
Misranur Yazgan <yazganmisra@gmail.com>
2020-04-06 14:53:50 +0300

    Update SphereGen.h

--- HW2/HW2/SphereGen.h
+++ HW2/HW2/SphereGen.h
@@ -8,10 +8,6 @@ class SphereGen
 public:
 	SphereGen() {};
 	Mesh * GenerateSphere();
-	vector<int> FindPoleVertices(Mesh *mesh);
-	void FindCutVertices(Mesh *mesh, vector<int> &cutVertices);
-	pair<map<int, int>, set<int>> CreateCut(Mesh *mesh, vector<int> &cutVertices, vector<int> &duplicateVertices);
-	int GetIndex(const vector<int> cutVertices, int v);
 private:
 	FreeVertex *centroid;
 	FreeVertex* FindCentroid(Mesh *mesh);
@@ -20,9 +16,4 @@ private:
 	float * GetMiddlePoint(float *v1coords, float *v2coords);
 	Mesh * CreateTetrahedron();
 	void SplitTetrahedron(Mesh *mesh, int level);
-	void FillLabels(map<int, int> triLabels, vector<int> &labeled0, vector<int> &labeled1);
-	void FillCutsNonCuts(Mesh *mesh, vector<int> &cuts, vector<int> &noncuts, vector<int> cutVertices, int triId);
-	
-	vector<int> FindCommonVertices(Mesh *mesh, int t1, int t2);
-	bool IsCutEdge(vector<int> cutVertices, vector<int> verts);
 };
